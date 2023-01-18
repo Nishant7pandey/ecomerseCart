8dccfe586348507e665b1da294d13951a79da2db
Nishant Pandey <109078897+Nishant7pandey@users.noreply.github.com>
2023-01-19 03:29:43 +0530

    hello

--- app/src/components/Home/Search/index.js
+++ app/src/components/Home/Search/index.js
@@ -4,7 +4,7 @@ import './Style.css';
 const Search = (props) => {
     return (
         <div className="d-flex align-items-center pt-80">
-            <span className="search">Search Courses</span>
+            <span className="search">Search </span>
             <input className="form-control" type="search" placeholder="Search" aria-label="Search" onChange={(event) => {
                 props.setFilteredCards(event.target.value, "search")
             }} />
